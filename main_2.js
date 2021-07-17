b950524fc7b5614b6740bfd7e6c103ad03852c1d
PrinceOfGit <ShandorUk30@yahoo.com>
2021-07-17 18:41:24 +0300

    Wanna take some time off to recreate.

---
+++ main_2.js
@@ -0,0 +1,46 @@
+let array_1 = [
+
+    [0, 1, 45, 23, 56],
+    ['Hello', 'Luck', 'Food', 'Bro', 'Friend'],
+    [4===4, 2>3, 'Father', 24, true, 12===12],
+]
+
+console.log(array_1)
+
+
+let array_2 = [
+    str_1 = ['Food'],
+    str_2 = ['Madness'],
+    bull_1 = [13===14],
+    num_1 = [14],
+    num_2 = [65],
+]
+
+console.log(array_2[0])
+console.log(array_2[1])
+console.log(array_2[2])
+console.log(array_2[3])
+console.log(array_2[4])
+
+
+
+for (let i = 0; i < 10; i++) {
+    document.write('<div>' + '<p>' + 'Hello' + '</p>' + '</div>')
+}
+
+for (let i = 0; i < 10; i++) {
+    document.write('<div>' + '<p>' + 'Hello' + '</p>' + i + '</div>')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
