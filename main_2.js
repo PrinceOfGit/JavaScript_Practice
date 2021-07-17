98716165625d1638e3e19d1df0f3ab3d6bfd126b
PrinceOfGit <ShandorUk30@yahoo.com>
2021-07-17 22:20:22 +0300

    Commit just in case

--- main_2.js
+++ main_2.js
@@ -33,6 +33,61 @@ for (let i = 0; i < 10; i++) {
 }
 
 
+let x = 0
+while (x < 20) {
+    document.write('<div>' + '<h1>' + 'Teemo' + ' ' + 'is' + ' ' + 'cool' + '</h1>' + '</div>')
+
+        x++;
+}
+
+
+let y = 0
+while (y < 20) {
+    document.write('<div>' + '<h1>' + 'Teemo' + ' ' + 'is' + ' ' + 'cool' + '</h1>' + y + '</div>')
+
+    y++;
+}
+
+
+let numbers = [1, 4, 56, 76, 89, 33, 67, 456, 789, 98]
+for (let j = 0; j < 10; j++) {
+
+    console.log(numbers[j])
+}
+
+
+let yammering = ['Great', 'Money', 'Sneak', 'Tiger', 'King', 'Walrus', 'Witcher', 'Wizard', 'Ice', 'Fire', 'Sky']
+let p = 0
+while (p < 10) {
+    p++
+
+    console.log(yammering[p])
+}
+
+let row_of_differences = [1, 4>5, 90===90, 'Baby', 'Lineage', 54, 13, 90!=30, 'Dog', 45, true]
+let r = 0
+while (r < 10) {
+    r++
+    console.log(row_of_differences[r])
+}
+
+
+let new_row_1 = [34, 'Jam', 'Kiss', 4>4, 5>=5, 'Silly', 46, 'Cat', 3290, 'Pipe']
+for (o = 0; o < new_row_1.length; o++) {
+    if (typeof new_row_1[o] === 'boolean') {
+        console.log(new_row_1[o])
+    }
+}
+
+let new_row_2 = ['Mother', 'Ship', 456, 789, 98<=45, 'Ultimate', 'Lion', 67, 3>6, 'Nuts']
+let m = 0
+while (m < new_row_2.length) {
+    if (typeof new_row_2[m] === 'string') {
+        console.log(new_row_2[m])
+    }
+    m ++
+}
+
 
 
 
