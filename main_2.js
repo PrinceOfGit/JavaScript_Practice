269dcaed1604a0115363dd0328d3c06ced35041c
PrinceOfGit <ShandorUk30@yahoo.com>
2021-07-19 18:56:48 +0300

    I finished doing 4 exercises + made some changes

---
+++ main_2.js
@@ -0,0 +1,60 @@
+//function square (h, w) {
+//return h * w
+ //}
+
+//let result = square(10, 10)
+//console.log(result)
+
+
+//function circle (radius) {
+//return Math.pow(radius, 2) * Math.PI
+//}
+
+//let result = circle(100)
+//console.log(result)
+
+//потому что S=pi * r2 (квадрате)
+
+//function cylindre (radius, height) {
+//return 2 * Math.PI * radius * height
+//}
+
+//let result = cylindre(100, 100)
+//console.log(result)
+
+
+//function blocks_1 (header){
+//document.write('<div>' +
+//'<h1>' + {header} + '</h1>' +
+//'</div>')
+//}
+
+//blocks_1('where is my money?')
+
+
+//function row_1 (text) {
+    //document.write('<div>' +
+        //'<ul>' +
+        //'<li>' + text + '</li>' +
+        //'<li>' + text + '</li>' +
+        //'<li>' + text + '</li>' +
+        //'</ul>' +
+        //'</div>')
+//}
+
+//row_1('Where is my bro?')
+
+
+//function row_2 (text) {
+    //for (let i = 0; i < 10; i++) {
+        //document.write('<ul>' + '<li>' + text + '</li>' + i + '</ul>')
+    //}
+//}
+
+//row_2('Teemo is your bro')
+
+
+
+
+
+
