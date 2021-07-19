026d34df48488df5a9943474481f5568b8de3ae1
PrinceOfGit <ShandorUk30@yahoo.com>
2021-07-19 16:33:27 +0300

    I finished doing exercises + made some changes

--- main_2.js
+++ main_2.js
@@ -1,115 +1,115 @@
-let array_1 = [
+//let array_1 = [
 
-    [0, 1, 45, 23, 56],
-    ['Hello', 'Luck', 'Food', 'Bro', 'Friend'],
-    [4===4, 2>3, 'Father', 24, true, 12===12],
-]
+    //[0, 1, 45, 23, 56],
+    //['Hello', 'Luck', 'Food', 'Bro', 'Friend'],
+    //[4===4, 2>3, 'Father', 24, true, 12===12],
+//]
 
-console.log(array_1)
-
-
-let array_2 = [
-    str_1 = ['Food'],
-    str_2 = ['Madness'],
-    bull_1 = [13===14],
-    num_1 = [14],
-    num_2 = [65],
-]
-
-console.log(array_2[0])
-console.log(array_2[1])
-console.log(array_2[2])
-console.log(array_2[3])
-console.log(array_2[4])
+//console.log(array_1)
 
 
+//let array_2 = [
+    //str_1 = ['Food'],
+    //str_2 = ['Madness'],
+    //bull_1 = [13===14],
+    //num_1 = [14],
+    //num_2 = [65],
+//]
 
-for (let i = 0; i < 10; i++) {
-    document.write('<div>' + '<p>' + 'Hello' + '</p>' + '</div>')
-}
-
-for (let i = 0; i < 10; i++) {
-    document.write('<div>' + '<p>' + 'Hello' + '</p>' + i + '</div>')
-}
-
-
-let x = 0
-while (x < 20) {
-    document.write('<div>' + '<h1>' + 'Teemo' + ' ' + 'is' + ' ' + 'cool' + '</h1>' + '</div>')
-
-        x++;
-}
+//console.log(array_2[0])
+//console.log(array_2[1])
+//console.log(array_2[2])
+//console.log(array_2[3])
+//console.log(array_2[4])
 
 
-let y = 0
-while (y < 20) {
-    document.write('<div>' + '<h1>' + 'Teemo' + ' ' + 'is' + ' ' + 'cool' + '</h1>' + y + '</div>')
 
-    y++;
-}
+//for (let i = 0; i < 10; i++) {
+    //document.write('<div>' + '<p>' + 'Hello' + '</p>' + '</div>')
+//}
 
-
-let numbers = [1, 4, 56, 76, 89, 33, 67, 456, 789, 98]
-for (let j = 0; j < 10; j++) {
-
-    console.log(numbers[j])
-}
+//for (let i = 0; i < 10; i++) {
+    //document.write('<div>' + '<p>' + 'Hello' + '</p>' + i + '</div>')
+//}
 
 
-let yammering = ['Great', 'Money', 'Sneak', 'Tiger', 'King', 'Walrus', 'Witcher', 'Wizard', 'Ice', 'Fire', 'Sky']
-let p = 0
-while (p < 10) {
-    p++
+//let x = 0
+//while (x < 20) {
+    //document.write('<div>' + '<h1>' + 'Teemo' + ' ' + 'is' + ' ' + 'cool' + '</h1>' + '</div>')
 
-    console.log(yammering[p])
-}
-
-let row_of_differences = [1, 4>5, 90===90, 'Baby', 'Lineage', 54, 13, 90!=30, 'Dog', 45, true]
-let r = 0
-while (r < 10) {
-    r++
-    console.log(row_of_differences[r])
-}
+        //x++;
+//}
 
 
-let new_row_1 = [34, 'Jam', 'Kiss', 4>4, 5>=5, 'Silly', 46, 'Cat', 3290, 'Pipe']
-for (o = 0; o < new_row_1.length; o++) {
-    if (typeof new_row_1[o] === 'boolean') {
-        console.log(new_row_1[o])
-    }
-}
+//let y = 0
+//while (y < 20) {
+    //document.write('<div>' + '<h1>' + 'Teemo' + ' ' + 'is' + ' ' + 'cool' + '</h1>' + y + '</div>')
 
-let new_row_2 = ['Mother', 'Ship', 456, 789, 98<=45, 'Ultimate', 'Lion', 67, 3>6, 'Nuts']
-let m = 0
-while (m < new_row_2.length) {
-    if (typeof new_row_2[m] === 'string') {
-        console.log(new_row_2[m])
-    }
-    m ++
-}
+    //y++;
+//}
 
-let new_row_3 = [3234, 54, 'Tea', 59, 'Cream', 'jar', 78, 4===4, 90, 'Sea']
-for (let q = 0; q < new_row_3.length; q++) {
-    if (typeof new_row_3[q] === 'number') {
-        console.log(new_row_3[q])
-    }
-}
 
-let array_3 = [];
-array_3[0] = 'Boomer';
-array_3[1] = 'Cross';
-array_3[2] = 9>8;
-array_3[3] = 34;
-array_3[4] = 90===90;
-array_3[5] = 'Max';
-array_3[6] = 367;
-array_3[7] = 32;
-array_3[8] = 'Child';
-array_3[9] = 'Prop';
+//let numbers = [1, 4, 56, 76, 89, 33, 67, 456, 789, 98]
+//for (let j = 0; j < 10; j++) {
 
-for (let t = 0; t < array_3.length; t++) {
-    console.log(array_3[t])
-}
+    //console.log(numbers[j])
+//}
+
+
+//let yammering = ['Great', 'Money', 'Sneak', 'Tiger', 'King', 'Walrus', 'Witcher', 'Wizard', 'Ice', 'Fire', 'Sky']
+//let p = 0
+//while (p < 10) {
+    //p++
+
+    //console.log(yammering[p])
+//}
+
+//let row_of_differences = [1, 4>5, 90===90, 'Baby', 'Lineage', 54, 13, 90!=30, 'Dog', 45, true]
+//let r = 0
+//while (r < 10) {
+    //r++
+    //console.log(row_of_differences[r])
+//}
+
+
+//let new_row_1 = [34, 'Jam', 'Kiss', 4>4, 5>=5, 'Silly', 46, 'Cat', 3290, 'Pipe']
+//for (o = 0; o < new_row_1.length; o++) {
+    //if (typeof new_row_1[o] === 'boolean') {
+        //console.log(new_row_1[o])
+    //}
+//}
+
+//let new_row_2 = ['Mother', 'Ship', 456, 789, 98<=45, 'Ultimate', 'Lion', 67, 3>6, 'Nuts']
+//let m = 0
+//while (m < new_row_2.length) {
+    //if (typeof new_row_2[m] === 'string') {
+        //console.log(new_row_2[m])
+    //}
+    //m ++
+//}
+
+//let new_row_3 = [3234, 54, 'Tea', 59, 'Cream', 'jar', 78, 4===4, 90, 'Sea']
+//for (let q = 0; q < new_row_3.length; q++) {
+    //if (typeof new_row_3[q] === 'number') {
+        //console.log(new_row_3[q])
+    //}
+//}
+
+//let array_3 = [];
+//array_3[0] = 'Boomer';
+//array_3[1] = 'Cross';
+//array_3[2] = 9>8;
+//array_3[3] = 34;
+//array_3[4] = 90===90;
+//array_3[5] = 'Max';
+//array_3[6] = 367;
+//array_3[7] = 32;
+//array_3[8] = 'Child';
+//array_3[9] = 'Prop';
+
+//for (let t = 0; t < array_3.length; t++) {
+    //console.log(array_3[t])
+//}
 
 
 //- Створити цикл for на 10  ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
@@ -118,10 +118,32 @@ for (let t = 0; t < array_3.length; t++) {
 //- Створити цикл for на 100 ітерацій. Вивести тільки парні кроки. через console.log + document.write
 //- Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write
 
-for (let xp = 0; xp < 10; xp++ ) {
-    console.log(`step :`, xp);
-    document.write(`step :`, xp)
-}
+
+//<br> - перенос
+
+//for (let xp = 0; xp < 10; xp++ ) {
+    //console.log(`step :`, xp);
+    //document.write(`step :`, xp)
+//}
+
+//for (let i = 0; i < 100; i++) {
+    //console.log(`step :`, i);
+    //document.write(`step :`, + i + `<br>`);
+//}
+
+//for (let i = 0; i < 100; i+=2) {
+    //console.log(`step :`, i);
+    //document.write(`step :`, + i + `<br>`);
+//}
+
+//for (let i = 0; i < 100; i++) {
+    //if (i%2 === 1) {
+        //console.log(`step :`, i);
+        //.write(`step :`, + i + `<br>`);
+//}
+//}
+
+
 
 
 
